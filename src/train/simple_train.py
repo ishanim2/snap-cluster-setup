69b3157443179df5a60f32b324747c69f312a0b0
ishanim2 <166900524+ishanim2@users.noreply.github.com>
2024-04-16 16:17:07 -0700

    Update simple_train.py

--- src/train/simple_train.py
+++ src/train/simple_train.py
@@ -129,7 +129,7 @@ def setup_and_train_proofnet(pretrained_model_name_or_path: str = "gpt2",
         logging_strategy='epoch',
         # lr_scheduler_type=lr_scheduler_type  # TODO: https://discord.com/channels/879548962464493619/1227708244697284724/1227708244697284724
         # warmup_ratio=warmup_ratio,
-        report_to = wandb,  # options I recommend: 'none', 'wandb'
+        report_to = 'wandb',  # options I recommend: 'none', 'wandb'
         fp16=False,  # never ever set to True
         bf16=torch.cuda.get_device_capability(torch.cuda.current_device())[0] >= 8,  # if >= 8 ==> brain float 16 available or set to True if you always want fp32
     )
